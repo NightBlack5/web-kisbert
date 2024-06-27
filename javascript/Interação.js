bc3d61e6ecff615270d11752f970d5910c6070fa
NightBlack5 <fernandofernandessantos123@gmail.com>
2024-06-27 14:59:50 +0000

    salve

---
+++ javascript/Interação.js
@@ -0,0 +1,45 @@
+const carouselContainer = document.querySelector('.carousel-container');
+        const carouselItems = document.querySelectorAll('.carousel-item');
+        const prevBtn = document.getElementById('prevBtn');
+        const nextBtn = document.getElementById('nextBtn');
+        const totalItems = carouselItems.length;
+        let currentIndex = 0;
+        let interval;
+
+        // Função para avançar para o próximo slide
+        function nextSlide() {
+            currentIndex = (currentIndex + 1) % totalItems;
+            updateCarousel();
+        }
+
+        // Função para voltar para o slide anterior
+        function prevSlide() {
+            currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+            updateCarousel();
+        }
+
+        // Função para atualizar a posição do carousel
+        function updateCarousel() {
+            const newPosition = -currentIndex * 100 + '%';
+            carouselContainer.style.transform = `translateX(${newPosition})`;
+        }
+
+        // Event listeners para os botões de controle
+        nextBtn.addEventListener('click', nextSlide);
+        prevBtn.addEventListener('click', prevSlide);
+
+        // Carrossel automático
+        function startCarousel() {
+            interval = setInterval(nextSlide, 3000); // Troca de imagem a cada 3 segundos (3000 ms)
+        }
+
+        function stopCarousel() {
+            clearInterval(interval);
+        }
+
+        // Iniciar carrossel automaticamente ao carregar a página
+        startCarousel();
+
+        // Pausar carrossel quando o mouse estiver sobre o carrossel
+        carousel.addEventListener('mouseover', stopCarousel);
+        carousel.addEventListener('mouseleave', startCarousel);
